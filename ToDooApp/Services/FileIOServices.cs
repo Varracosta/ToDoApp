6b8916087d565aae993278d1f4d056f3f1a6a737
Stacey Radushinskaya <starwisperer@gmail.com>
2020-10-30 20:16:04 +1300

    Add project files.

---
+++ ToDooApp/Services/FileIOServices.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Security.Policy;
+using System.Text;
+using ToDooApp.Models;
+
+namespace ToDooApp.Services
+{
+    class FileIOServices
+    {
+        private readonly string PATH;
+
+        public FileIOServices(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<ToDoModel> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
